45a6f3aebac9e9d4850a4aac5f5206e52577497d
bearwithtea <132114816+bearwithtea@users.noreply.github.com>
2024-02-29 13:48:20 -0600

    I do not understand how react works

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,25 @@
 import { Detail } from "@raycast/api";
-import "./src/static/style.css";
+import { motion } from "framer-motion";
+import './style.css';
+import React from "react";
 
-export default function Command() {
-  return <Detail markdown="# hehe" />;
+export default function Main() {
+  return (
+    <>
+      <Detail markdown="# Meditation" />
+      <Box />
+    </>
+  );
 }
+
+class MyHeader extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1 style={{color: "red"}}>Hello Style!</h1>
+      <p>Add a little style!</p>
+      </div>
+    );
+  }
+}
+
