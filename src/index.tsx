5d372e541d3e58874016bd53003923a19dfb97d7
bearwithtea <132114816+bearwithtea@users.noreply.github.com>
2024-03-01 16:19:23 -0600

    Done.

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,11 @@ export default function () {
   const everything =
   ` 
   # Breathe
-  ### As the bar moves upwards, inhale. As it retreats, exhale. 
+  ### As the bar moves right, inhale. As it moves left, exhale. 
                             
- ![Alt Text](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeG10aHZiaXdwbWg1b215eWFibXJkNW04NzFoeGtqczJkcGFwcmxleiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/qaojPCJEusQLpYQa7o/giphy.gif)
+ ![Breathing Gif](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYndmeHlldnA3Zm80dXcwajBobzJuN3JqbnFydGprNnpxYzBoNnE2dCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/0cYs8RisLWOjg6Dc6l/giphy.gif)
  
-“Nowhere can man find a quieter or more untroubled retreat than in his own soul.” – Marcus Aurelius
-  `;
+`
 
   return (
     <Detail markdown={everything} />
