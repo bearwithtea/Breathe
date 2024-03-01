e7d4e911976b8549967e004d4cfd8bac9a286a3c
bearwithtea <132114816+bearwithtea@users.noreply.github.com>
2024-03-01 11:35:23 -0600

    BY GOD WE ARE COOKING NOW

--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,18 @@ import { Detail } from "@raycast/api";
 import ReactMarkdown from "react-markdown";
 
 
+export default function () {
 
-  const markdown =
-  `
-    |    |
-    |    |
-    |    |
-    |    |
-    |    |
-    |    |
-    |    |
-    |    |
-    |    |
-    |____|
+  const everything =
+  ` 
+  # Breathe
+  ### As the bar moves upwards, inhale. As it retreats, exhale. 
+                            
+ ![Alt Text](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeG10aHZiaXdwbWg1b215eWFibXJkNW04NzFoeGtqczJkcGFwcmxleiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/qaojPCJEusQLpYQa7o/giphy.gif)
+        “Nowhere can man find a quieter or more untroubled retreat than in his own soul.” – Marcus Aurelius
   `;
 
-export default function () {
-  return <Detail markdown={markdown} />;
+  return (
+    <Detail markdown={everything} />
+  ) 
 }
