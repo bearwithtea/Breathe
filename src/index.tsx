8318627e6304cd90e65633f3fb1c6828c83b18e0
bearwithtea <132114816+bearwithtea@users.noreply.github.com>
2024-03-01 14:29:29 -0600

    getting there

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ export default function () {
   ### As the bar moves upwards, inhale. As it retreats, exhale. 
                             
  ![Alt Text](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeG10aHZiaXdwbWg1b215eWFibXJkNW04NzFoeGtqczJkcGFwcmxleiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/qaojPCJEusQLpYQa7o/giphy.gif)
+ 
         “Nowhere can man find a quieter or more untroubled retreat than in his own soul.” – Marcus Aurelius
   `;
 
