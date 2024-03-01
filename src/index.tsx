5ed28d76236a11cdc3d3f0799eb087aceb78dfe7
bearwithtea <132114816+bearwithtea@users.noreply.github.com>
2024-03-01 14:34:13 -0600

    created all figures

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ export default function () {
                             
  ![Alt Text](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeG10aHZiaXdwbWg1b215eWFibXJkNW04NzFoeGtqczJkcGFwcmxleiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/qaojPCJEusQLpYQa7o/giphy.gif)
  
-        “Nowhere can man find a quieter or more untroubled retreat than in his own soul.” – Marcus Aurelius
+“Nowhere can man find a quieter or more untroubled retreat than in his own soul.” – Marcus Aurelius
   `;
 
   return (
